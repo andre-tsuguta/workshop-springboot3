fcef5da7f5c63a7a030e1847e0bd237334970359
Andre Tsuguta <andre.mat@gmail.com>
2024-04-29 14:20:08 -0300

    User entity and resource

---
+++ src/main/java/com/andretsuguta/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.andretsuguta.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andretsuguta.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "992922922", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
