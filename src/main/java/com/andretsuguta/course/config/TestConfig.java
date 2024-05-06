c47eba699a3934d5b4ff73345d775a84ef490854
Andre Tsuguta <andre.mat@gmail.com>
2024-05-06 13:41:34 -0300

    Payment, one-to-one association

--- src/main/java/com/andretsuguta/course/config/TestConfig.java
+++ src/main/java/com/andretsuguta/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.andretsuguta.course.entities.Category;
 import com.andretsuguta.course.entities.Order;
 import com.andretsuguta.course.entities.OrderItem;
+import com.andretsuguta.course.entities.Payment;
 import com.andretsuguta.course.entities.Product;
 import com.andretsuguta.course.entities.User;
 import com.andretsuguta.course.entities.enums.OrderStatus;
@@ -79,9 +80,10 @@ public class TestConfig  implements CommandLineRunner{
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
-		orderItemRepository.saveAllAndFlush(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
-
-	
 }
