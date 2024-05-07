224d093eee0f61269f00ab36bd34e06afa633ed4
Andre Tsuguta <andre.mat@gmail.com>
2024-05-07 14:20:02 -0300

    Exception handling - update

--- src/main/java/com/andretsuguta/course/services/UserService.java
+++ src/main/java/com/andretsuguta/course/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.andretsuguta.course.entities.User;
@@ -13,28 +12,30 @@ import com.andretsuguta.course.repositories.UserRepository;
 import com.andretsuguta.course.services.exceptions.DatabaseException;
 import com.andretsuguta.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete (Long id) {
+
+	public void delete(Long id) {
 		try {
-			if ( ! repository.existsById(id)) {
+			if (!repository.existsById(id)) {
 				throw new ResourceNotFoundException(id);
 			}
 			repository.deleteById(id);
@@ -42,17 +43,21 @@ public class UserService {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		
-		User entity = repository.getReferenceById(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateDate(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException  e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDate(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());	
+		entity.setPhone(obj.getPhone());
 	}
 }
